f639731153a473cf84e8d51f5cbc1a6359d4da7a
ftshang <74879078+ftshang@users.noreply.github.com>
2023-12-10 22:29:53 -0500

    Updated - need to fix distance calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,142 @@
-#include "mbed.h"
-
-#define CTRL_REG1 0x20
-#define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
-
-#define CTRL_REG4 0x23
-#define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
+#include <mbed.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include "drivers/LCD_DISCO_F429ZI.h"
 
 #define OUT_X_L 0x28
+#define CTRL_REG1 0x20
+#define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
+#define CTRL_REG4 0x23
+#define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
 #define SPI_FLAG 1
+#define SCALING_FACTOR (17.5f * 0.017453292519943295769236907684886f / 1000.0f);
+#define LEG_LENGTH (0.95)
+#define FILTER_COEFFICIENT 0.1f
 
-#define SCALING_FACTOR (17.5f * 0.17453292519943295769236907684886f / 1000.0f)
-
+SPI spi(PF_9, PF_8, PF_7, PC_1, use_gpio_ssel);
+uint8_t write_buf[32];
+uint8_t read_buf[32];
+float data_buf[40];
+Ticker t;
+DigitalIn button(PA_0);
 EventFlags flags;
 
+// SPI callback used for transfer function.
 void spi_cb(int event)
 {
     flags.set(SPI_FLAG);
 }
 
-int main()
+// Ticker callback.
+void cb()
 {
-    SPI spi(PF_9, PF_8, PF_7, PC_1, use_gpio_ssel);
+    flags.set(SPI_FLAG);
+}
 
-    uint8_t write_buf[32], read_buf[32];
-
+void setupGyroscope()
+{
     spi.format(8, 3);
     spi.frequency(1'000'000);
-
     write_buf[0] = CTRL_REG1;
     write_buf[1] = CTRL_REG1_CONFIG;
-
-    spi.transfer(write_buf, 2, read_buf, 2, spi_cb);
+    spi.transfer(write_buf, 2, read_buf, 2, spi_cb, SPI_EVENT_COMPLETE);
     flags.wait_all(SPI_FLAG);
 
     write_buf[0] = CTRL_REG4;
     write_buf[1] = CTRL_REG4_CONFIG;
-
-    spi.transfer(write_buf, 2, read_buf, 2, spi_cb);
+    spi.transfer(write_buf, 2, read_buf, 2, spi_cb, SPI_EVENT_COMPLETE);
     flags.wait_all(SPI_FLAG);
+}
 
-    write_buf[1] = 0xFF;
+void readData()
+{
+    write_buf[0] = OUT_X_L | 0x80 | 0x40;
+    spi.transfer(write_buf, 7, read_buf, 7, spi_cb, SPI_EVENT_COMPLETE);
+    flags.wait_all(SPI_FLAG);
+}
+
+int main()
+{
+    LCD_DISCO_F429ZI lcd;
+    lcd.Clear(LCD_COLOR_BLACK);
+    lcd.SetTextColor(LCD_COLOR_RED);
+    lcd.SetBackColor(LCD_COLOR_BLACK);
+    lcd.DisplayStringAtLine(0, (uint8_t *)"Welcome.");
+    lcd.DisplayStringAtLine(1, (uint8_t *)"Please press the blue");
+    lcd.DisplayStringAtLine(2, (uint8_t *)"button to begin.");
+
+    bool startMenu = true;
+
+    while (startMenu)
+    {
+        if (button.read() == 1)
+        {
+            startMenu = false;
+        }
+    }
+
+    int count = 0;
+    int16_t raw_gx, raw_gy, raw_gz;
+    float gx, gy, gz;
+    float filtered_gx, filtered_gy, filtered_gz = 0.0f;
+    t.attach(&cb, 500ms);
+    setupGyroscope();
+
+    lcd.Clear(LCD_COLOR_BLACK);
+    lcd.SetTextColor(LCD_COLOR_RED);
+    lcd.SetBackColor(LCD_COLOR_BLACK);
+    lcd.DisplayStringAtLine(0, (uint8_t *)"Begin walking now.");
+
+    printf("start\n");
 
     while (1)
     {
-        int16_t raw_gx, raw_gy, raw_gz;
-        float gx, gy, gz;
-
-        write_buf[0] = OUT_X_L | 0x80 | 0x40;
-
-        spi.transfer(write_buf, 7, read_buf, 7, spi_cb);
         flags.wait_all(SPI_FLAG);
 
+        if (count == 40)
+        {
+            break;
+        }
+        readData();
         raw_gx = (((uint16_t)read_buf[2]) << 8) | ((uint16_t)read_buf[1]);
         raw_gy = (((uint16_t)read_buf[4]) << 8) | ((uint16_t)read_buf[3]);
         raw_gz = (((uint16_t)read_buf[6]) << 8) | ((uint16_t)read_buf[5]);
-        printf("RAW -> \t\tgx: %d \t gy: %d\t gz: %d\t\n", raw_gx, raw_gy, raw_gz);
 
-        printf(">x_axis: %d|g\n", raw_gx);
-        printf(">y_axis: %d|g\n", raw_gy);
-        printf(">z_axis: %d|g\n", raw_gz);
+        gx = ((float)raw_gx) * SCALING_FACTOR;
+        gy = ((float)raw_gy) * SCALING_FACTOR;
+        gz = ((float)raw_gz) * SCALING_FACTOR;
 
-        gx = ((float) raw_gx) * SCALING_FACTOR;
-        gy = ((float) raw_gy) * SCALING_FACTOR;
-        gz = ((float) raw_gz) * SCALING_FACTOR;
+        // Applying low pass filter to reduce noise.
+        filtered_gx = FILTER_COEFFICIENT * gx + (1 - FILTER_COEFFICIENT) * filtered_gx;
+        filtered_gy = FILTER_COEFFICIENT * gy + (1 - FILTER_COEFFICIENT) * filtered_gy;
+        filtered_gz = FILTER_COEFFICIENT * gz + (1 - FILTER_COEFFICIENT) * filtered_gz;
 
-        printf("Actual ->\tgx: %4.5f \t gy: %4.5f \t gz: %4.5f\n\n", gx, gy, gz);
-        thread_sleep_for(100);
+        printf("Filtered|\tfiltered_gx: %4.5f \t filtered_gy: %4.5f \t filtered_gz: %4.5f\n", filtered_gx, filtered_gy, filtered_gz);
+        data_buf[count] = filtered_gz;
+        count += 1;
     }
+
+    float totalRadians = 0;
+    for (int i = 0; i < 40; i += 1)
+    {
+        totalRadians += data_buf[i];
+    }
+
+    float linearVelocity = totalRadians * LEG_LENGTH;
+
+    char buf[20];
+    char second_buf[20];
+
+    lcd.Clear(LCD_COLOR_BLACK);
+    lcd.SetTextColor(LCD_COLOR_RED);
+    lcd.SetBackColor(LCD_COLOR_BLACK);
+
+    sprintf(buf, "%f", linearVelocity);
+
+    char str[] = " meters.";
+    strcat(buf, str);
+
+    lcd.DisplayStringAtLine(0, (uint8_t *)"Total Distance: ");
+    lcd.DisplayStringAtLine(1, (uint8_t *)buf);
 }
